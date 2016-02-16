cfcfbdbffd749271ab9c1dfc88f5c02af9d1dc6a
Damien Leroy <damien7leroy@gmail.com>
2016-02-16 12:23:01 +0100

    x^p modulo n

--- euclide.py
+++ euclide.py
@@ -1,3 +1,5 @@
+import math
+
 def euclide(a, b) : 
 	if a < b or b < 1 : 
 		return None
@@ -14,6 +16,30 @@ def inverse(n, m) :
 	""" Retourne l'inverse de n modulo m """
 	x, y = euclide(m, n)
 	return y
+
+def restexpmn(x, p, n) : 
+	""" Retourne le reste de x^p modulo n """
+	resultat = p
+	compteur = 1
+	i = 1
+	while resultat != 1 : 
+		resultat = resultat / 2
+		compteur = compteur * 2
+		i = i + 1
+	resultat = [0] * (i)
+	for j in range(0, i+1) : 
+		if p > 0 and compteur <= p : 
+			resultat[(i-j)-1] = 1
+			p = p - compteur
+		compteur = compteur / 2
+	final = x%n
+	r = 1
+	for j in range(0, i) : 
+		if j>0 : 
+			final = (final*final) % n
+		if resultat[j] == 1 : 
+			r = (r * final) % n
+	return r
 		
 if __name__ == '__main__':
 	print "### Algorithme d'Euclide ###"
@@ -28,3 +54,11 @@ if __name__ == '__main__':
 	m = 242
 	y = inverse(n, m)
 	print "y=" + str(y)
+	
+	print "### Reste de x^p modulo n ###"
+	x = 7
+	p = 4
+	n = 11
+	r = restexpmn(x, p, n)
+	print "x=" + str(x) + ", p=" + str(p) + ", n=" + str(n)
+	print "r=" + str(r)
--- testEuclide.py
+++ testEuclide.py
@@ -19,6 +19,63 @@ class TestEuclide(unittest.TestCase) :
 	def test_inverse2(self) :
 		resultat = inverse(51, 242)
 		self.assertEqual(resultat, 19)
+	
+	
+	def test_restexpmn1(self) :
+		resultat = restexpmn(7, 2, 11)
+		self.assertEqual(resultat, 5)
+	
+	def test_restexpmn2(self) :
+		resultat = restexpmn(7, 4, 11)
+		self.assertEqual(resultat, 3)
+	
+	def test_restexpmn3(self) :
+		resultat = restexpmn(7, 8, 11)
+		self.assertEqual(resultat, 9)
+	
+	def test_restexpmn4(self) :
+		resultat = restexpmn(7, 16, 11)
+		self.assertEqual(resultat, 4)
+	
+	def test_restexpmn5(self) :
+		resultat = restexpmn(7, 32, 11)
+		self.assertEqual(resultat, 5)
+	
+	def test_restexpmn6(self) :
+		resultat = restexpmn(7, 58, 11)
+		self.assertEqual(resultat, 9)
+	
+	def test_restexpmn7(self) :
+		resultat = restexpmn(23, 2, 91)
+		self.assertEqual(resultat, 74)
+	
+	def test_restexpmn8(self) :
+		resultat = restexpmn(23, 4, 91)
+		self.assertEqual(resultat, 16)
+	
+	def test_restexpmn9(self) :
+		resultat = restexpmn(23, 5, 91)
+		self.assertEqual(resultat, 4)
+	
+	def test_restexpmn10(self) :
+		resultat = restexpmn(4, 2, 91)
+		self.assertEqual(resultat, 16)
+	
+	def test_restexpmn11(self) :
+		resultat = restexpmn(4, 4, 91)
+		self.assertEqual(resultat, 74)
+	
+	def test_restexpmn12(self) :
+		resultat = restexpmn(4, 8, 91)
+		self.assertEqual(resultat, 16)
+	
+	def test_restexpmn13(self) :
+		resultat = restexpmn(4, 16, 91)
+		self.assertEqual(resultat, 74)
+	
+	def test_restexpmn14(self) :
+		resultat = restexpmn(4, 29, 91)
+		self.assertEqual(resultat, 23)
         
 
 if __name__ == '__main__':
